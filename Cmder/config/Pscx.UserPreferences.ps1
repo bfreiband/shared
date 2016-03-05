d8a9bc30c61ee13d5e4ad241cf2df783f9092680
Lori Holden <email@loriholden.com>
2016-03-04 23:04:02 -0800

    Don't rely on the powershell gallery as it wants to install modules outside of the home dir

---
+++ Cmder/config/Pscx.UserPreferences.ps1
@@ -0,0 +1,15 @@
+@{
+  ShowModuleLoadDetails = $false
+  CD_GetChildItem = $false
+  CD_EchoNewLocation = $false
+  ModulesToImport = @{
+    DirectoryServices = $false
+    GetHelp = $true
+    Prompt = $false
+    Net = $false
+    Utility = $true
+    Vhd = $false
+    Wmi = $false
+    FileSystem = $false
+  }
+}
